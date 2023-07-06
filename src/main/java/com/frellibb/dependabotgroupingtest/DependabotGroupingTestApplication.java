867416b1f19b20cd738e4ff48413e50a1cca0334
Francesco Borg Bonaci <francesco.borgbonaci@marshmallow.co>
2023-07-06 17:16:38 +0100

    initial commit

---
+++ src/main/java/com/frellibb/dependabotgroupingtest/DependabotGroupingTestApplication.java
@@ -0,0 +1,13 @@
+package com.frellibb.dependabotgroupingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotGroupingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependabotGroupingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frellibb/dependabotgroupingtest/DependabotGroupingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frellibb.dependabotgroupingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotGroupingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
